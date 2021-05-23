bd2b3858371827c62eaf3045fe12dc77ddd373a2
Camilo Andrés Morán López <camoran@udenar.edu.co>
2021-05-23 07:10:24 -0500

    He agregado un gitignore

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const colors = require('colors');
+
+const handleServer = function(req, res) {
+    res.writeHead(200, {
+        'Content-type': 'text/html'
+    });
+    res.write('Hola Mundo');
+    res.end();
+};
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function() {
+    console.log('Server on port 3000'.green);
+});
